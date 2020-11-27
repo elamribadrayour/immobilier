16ba9e2e775643ab1adf60b79a88de7c071e467c
elamri <badrayour.elamri@stilog.com>
2020-11-27 09:58:41 +0100

    [ADD] ignoring old

--- __old/src/States/States.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const useSemiPersistentState = (key, initialState) =>
-{
-  const [value, setValue] = React.useState(initialState);
-  // React.useEffect(() => { localStorage.setItem(key, value); }, [value, key]);    
-  return [value, setValue];
-}
